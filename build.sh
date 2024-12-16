57deeef947d27c0fc431ad1a75664904ece278db
Thalia Cantero <thal.cantero@gmail.com>
2024-12-15 20:16:52 -0500

    Dockerfiles and streamlit

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ echo "TODO: fill in the docker build command"
 
 # Build the Docker image for the serving app
 docker build -t ift6758-serving -f Dockerfile.serving .
+docker build -t ift6758-streamlit -f Dockerfile.streamlit .
--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 echo "TODO: fill in the docker run command"
 
 # Run the Docker container for the serving app
-docker run -p 8000:8000 -e WANDB_API_KEY=$WANDB_API_KEY ift6758-serving
+docker-compose up --build
